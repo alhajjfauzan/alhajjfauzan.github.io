f5f0e9c00ae2fcdf44d82afabdc942b2e3f57169
Al Hajj Fauzan <162179863+alhajjfauzan@users.noreply.github.com>
2024-10-02 22:44:07 +0800

    Add files via upload

---
+++ posttest4/tampilkan.php
@@ -0,0 +1,10 @@
+
+<?php
+    echo"Email".$_POST["emaile"];
+    echo "<br>";
+    echo"Nama Lengkap".$_POST["namalengkap"];
+    echo "<br>";
+    echo"Password".$_POST["pass"];
+    echo "<br>";
+    echo"Nomor telepon".$_POST["phone"];
+?>
