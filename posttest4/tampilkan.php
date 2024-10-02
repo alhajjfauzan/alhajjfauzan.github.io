a5eaeb8201e60c43e9f7cf5f8b044171f532869e
Al Hajj Fauzan <162179863+alhajjfauzan@users.noreply.github.com>
2024-10-02 22:29:12 +0800

    Update tampilkan.php

--- posttest4/tampilkan.php
+++ posttest4/tampilkan.php
@@ -4,7 +4,7 @@
     echo "<br>";
     echo"Nama Lengkap".$_POST["fullname"];
     echo "<br>";
-    echo"Password".$_POST["psw"];
+    echo"Password".$_POST["pawsword"];
     echo "<br>";
     echo"Nomor telepon".$_POST["phone"];
 ?>
